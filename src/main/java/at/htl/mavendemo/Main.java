658a6c05ee013e42d9442aa6bf27918406d9e15d
Elias Röbl <eliasroebl@gmail.com>
2019-06-24 15:15:03 +0200

    log4j & jsoup implemented

--- src/main/java/at/htl/mavendemo/Main.java
+++ src/main/java/at/htl/mavendemo/Main.java
@@ -1,8 +1,36 @@
 package at.htl.mavendemo;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
+
+    final static Logger logger = LogManager.getLogger(Main.class.getSimpleName());
 
     public static void main(String[] args){
         System.out.println("Hello 3AHIF!");
+        logger.info("Hello");
+        parsehtml("http://en.wikipedia.org/");
+    }
+
+    public static void parsehtml(String url){
+        try {
+            Document doc = Jsoup.connect(url).get();
+            logger.info(doc.title());
+            Elements newsHeadlines = doc.select("#mp-itn b a");
+            for (Element headline : newsHeadlines) {
+                System.out.printf("%s\t%s\n",
+                        headline.attr("title"),
+                        headline.absUrl("href"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
